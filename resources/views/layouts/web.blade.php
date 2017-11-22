cba8827ad4a1819cb8dc80677fe172e8d551023c
Daniel WS <danielws.x64@gmail.com>
2017-11-21 23:42:09 -0200

    Add locastyle assets into project

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -8,10 +8,12 @@
     <meta charset="utf-8">
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="description" content="Insira aqui a descrição da página.">
-    <link href="http://assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css" rel="stylesheet" type="text/css">
-    <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
-    <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
+    <meta name="description" content="@yield('page_descriptio')">
+
+    <link href="css/locastyle.css" rel="stylesheet" type="text/css">
+
+    <link rel="icon" sizes="192x192" href="">
+    <link rel="apple-touch-icon" href="">
   </head>
   <body>
 
@@ -25,7 +27,7 @@
     </main>
 
     <!-- We recommended use jQuery 1.10 or up -->
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="http://assets.locaweb.com.br/locastyle/3.10.1/javascripts/locastyle.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/locastyle.js" type="text/javascript"></script>
   </body>
 </html>
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.web')
+
+@section('page_title', 'Page title')
+
+@section('main')
+
+<div class="container-fluid">
+  <h1 class="ls-title-intro ls-ico-home">Página inicial</h1>
+  <p>the name is {{ $user->user_name }}</p>
+
+
+</div>
+
+@endsection
