703b4bcfd1760fb22203b979f187c4d9298e8888
Daniel WS <danielws.x64@gmail.com>
2017-11-21 23:19:48 -0200

    Create layout with locastyle

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html class="ls-theme-turquoise">
+  <head>
+    <title>
+      @yield('page_title')
+    </title>
+
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="Insira aqui a descrição da página.">
+    <link href="http://assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css" rel="stylesheet" type="text/css">
+    <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
+    <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
+  </head>
+  <body>
+
+    @include('layouts.web.topbar')
+    @include('layouts.web.sidebar')
+
+    <main class="ls-main ">
+
+      @yield('main')
+
+    </main>
+
+    <!-- We recommended use jQuery 1.10 or up -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="http://assets.locaweb.com.br/locastyle/3.10.1/javascripts/locastyle.js" type="text/javascript"></script>
+  </body>
+</html>
---
+++ resources/views/layouts/web/user-menu.blade.php
@@ -0,0 +1,17 @@
+<!-- Dropdown com detalhes da conta de usuário -->
+<div data-ls-module="dropdown" class="ls-dropdown ls-user-account">
+  <a href="#" class="ls-ico-user">
+    <img src="/locawebstyle/assets/images/locastyle/avatar-example.jpg" alt="" />
+    <span class="ls-name">João Kennedy</span>
+    (johnkennedy)
+  </a>
+
+  <nav class="ls-dropdown-nav ls-user-menu">
+    <ul>
+      <li><a href="#">Meus dados</a></li>
+      <li><a href="#">Faturas</a></li>
+      <li><a href="#">Planos</a></li>
+      <li><a href="#">Sair</a></li>
+      </ul>
+  </nav>
+</div>
